77d189e7203aeaf324bcc4a6a6f68f2873e9a3b7
JuanRamirezCatrileo <juanramirezcatrileo@gmail.com>
2022-12-02 13:21:27 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-piano';
+  aplicarsonido(numero: number): void{
+    const audio= new Audio();
+    audio.src='../assets/sonidos/note' + numero + '.wav';
+    audio.load();
+    audio.play();
+  }
+}
